3e5e84777c384fc729fb9baeb396a758e257a528
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-19 03:48:20 +0000

    commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,9 @@
 class DirectorsController < ApplicationController
   def index
     render ({ :template => "director_templates/list" })
+  end
 
+  def show
+    render ({ :template => "director_templates/details" })
   end
 end
